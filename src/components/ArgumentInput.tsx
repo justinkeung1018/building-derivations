ac0c66b4fc208afb1e48badcad94bd0ec1425c75
Justin Keung <justinkeung1018@gmail.com>
2024-12-19 23:30:59 +0800

    Focus input when editing from second time onwards

--- src/components/ArgumentInput.tsx
+++ src/components/ArgumentInput.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Input } from "@/components/shadcn/Input";
 import { useState } from "react";
 import { MathJax } from "better-react-mathjax";
 
+interface FocusingInputProps {
+  edited: boolean;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+}
+
+export default function FocusingInput({ edited, onBlur }: FocusingInputProps) {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (edited) {
+      ref.current?.focus();
+    }
+  }, []);
+
+  return <Input ref={ref} onBlur={onBlur} />;
+}
+
 export function ArgumentInput() {
   const [value, setValue] = useState("");
   const [isEditing, setIsEditing] = useState(true);
+
+  // Focus input when we try to edit from the second time onwards
+  const [edited, setEdited] = useState(false);
 
   const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -13,11 +33,12 @@ export function ArgumentInput() {
     console.log(value);
   };
 
-  return isEditing ? (
-    <Input onBlur={onBlur} />
+  return isEditing || value.length == 0 ? (
+    <FocusingInput edited={edited} onBlur={onBlur} />
   ) : (
     <MathJax
       onClick={() => {
+        setEdited(true);
         setIsEditing(true);
       }}
     >
