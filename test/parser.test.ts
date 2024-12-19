4bf0f096002197fecdc9aa0ebf21f9f91434b16a
Justin Keung <justinkeung1018@gmail.com>
2024-12-19 22:48:56 +0800

    Add some more parser tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,14 +1,25 @@
 import { Abstraction, Application, parse, Variable } from "@/lib/lexer";
 
+const x = new Variable("x");
+const y = new Variable("y");
+const lambdaXX = new Abstraction(x, x);
+const lambdaXY = new Abstraction(x, y);
+
 test("Parses variables", () => {
-  expect(parse("x")).toEqual(new Variable("x"));
+  expect(parse("x")).toEqual(x);
 });
 
 test("Parses abstractions", () => {
-  expect(parse("(\\x. x)")).toEqual(new Abstraction(new Variable("x"), new Variable("x")));
+  expect(parse("(\\x. x)")).toEqual(lambdaXX);
+  expect(parse("(\\x. y)")).toEqual(lambdaXY);
 });
 
 test("Parses applications", () => {
-  const lambdaXX = new Abstraction(new Variable("x"), new Variable("x"));
+  const x = new Variable("x");
+  const lambdaXX = new Abstraction(x, x);
   expect(parse("((\\x. x)(\\x. x))")).toEqual(new Application(lambdaXX, lambdaXX));
+  expect(parse("(xx)")).toEqual(new Application(x, x));
+  expect(parse("(xy)")).toEqual(new Application(x, y));
+  expect(parse("(x(\\x.x))")).toEqual(new Application(x, lambdaXX));
+  expect(parse("((\\x.    x)x)")).toEqual(new Application(lambdaXX, x));
 });
