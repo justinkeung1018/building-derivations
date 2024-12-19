953ff7d9f8623905f7a29ce4a75a2ce6cfb313f2
Justin Keung <justinkeung1018@gmail.com>
2024-12-19 12:18:21 +0800

    Initial setup

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    ignores: ["dist/", "eslint.config.mjs"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
