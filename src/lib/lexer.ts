956c33cee7332c5ec9988976ebbfe25195803253
Justin Keung <justinkeung1018@gmail.com>
2024-12-20 11:51:24 +0800

    Add comments

--- src/lib/lexer.ts
+++ src/lib/lexer.ts
@@ -70,14 +70,21 @@ function applyApplication(value: [Token<TokenKind.LParen>, Term, Term, Token<Tok
   return new Application(value[1], value[2]);
 }
 
+// Variable = x
 VARIABLE.setPattern(apply(tok(TokenKind.Variable), applyVariable));
+
+// Abstraction = (\x. Term)
 ABSTRACTION.setPattern(
   apply(
     seq(tok(TokenKind.LParen), tok(TokenKind.Lambda), VARIABLE, tok(TokenKind.Dot), TERM, tok(TokenKind.RParen)),
     applyAbstraction,
   ),
 );
+
+// Application = (Term Term)
 APPLICATION.setPattern(apply(seq(tok(TokenKind.LParen), TERM, TERM, tok(TokenKind.RParen)), applyApplication));
+
+// Term = Variable | Abstraction | Application
 TERM.setPattern(alt(VARIABLE, ABSTRACTION, APPLICATION));
 
 function lex(term: string): Token<TokenKind>[] {
