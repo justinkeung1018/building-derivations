b7ff325d7a6554408787143d4ea9cbcef95b6182
Justin Keung <justinkeung1018@gmail.com>
2024-12-20 21:07:48 +0800

    Remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { ArgumentInput, ArgumentInputState, getDefaultState } from "./components/ArgumentInput";
 import { MathJaxContext } from "better-react-mathjax";
 
 export function App() {
   const [states, setStates] = useState<Record<number, ArgumentInputState>>({ 0: getDefaultState(0) });
-
-  useEffect(() => {
-    console.log(states);
-  }, [states]);
 
   return (
     <MathJaxContext>
