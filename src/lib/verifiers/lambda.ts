541a9ce3c4e676e96fc1c6e63df331bb30ceb1cd
Justin Keung <justinkeung1018@gmail.com>
2024-12-21 14:54:40 +0800

    Add arrow introduction verifier

--- src/lib/verifiers/lambda.ts
+++ src/lib/verifiers/lambda.ts
@@ -1,18 +1,62 @@
 import isEqual from "lodash.isequal";
 import { Argument } from "../parsers/argument";
-import { Variable } from "../parsers/lambda";
+import { Abstraction, Variable } from "../parsers/lambda";
+import { Arrow } from "../parsers/type";
 
-function action(argument: Argument): boolean {
-  const left = argument.assignment.term;
-  if (!(left instanceof Variable)) {
+function action(conclusion: Argument, premises: Argument[]): boolean {
+  if (premises.length !== 0) {
     return false;
   }
-  for (const varAssignment of argument.context.varAssignments) {
-    if (isEqual(left, varAssignment.variable)) {
-      return isEqual(argument.assignment.type, varAssignment.type);
+
+  const term = conclusion.assignment.term;
+  if (!(term instanceof Variable)) {
+    return false;
+  }
+
+  for (const varAssignment of conclusion.context.varAssignments) {
+    if (isEqual(term, varAssignment.variable)) {
+      return isEqual(conclusion.assignment.type, varAssignment.type);
     }
   }
   return false;
 }
 
-export { action };
+function arrowIntroduction(conclusion: Argument, premises: Argument[]): boolean {
+  if (premises.length !== 1) {
+    return false;
+  }
+
+  const premise = premises[0];
+
+  const term = conclusion.assignment.term;
+  if (!(term instanceof Abstraction)) {
+    return false;
+  }
+
+  const arrowType = conclusion.assignment.type;
+  if (!(arrowType instanceof Arrow)) {
+    return false;
+  }
+
+  for (const varAssignment of conclusion.context.varAssignments) {
+    if (isEqual(term.variable, varAssignment.variable)) {
+      // Bound variable cannot appear free in context by Barendregt's convention
+      return false;
+    }
+  }
+
+  if (!isEqual(term.body, premise.assignment.term) || !isEqual(arrowType.right, premise.assignment.type)) {
+    return false;
+  }
+
+  for (const varAssignment of premise.context.varAssignments) {
+    if (isEqual(term.variable, varAssignment.variable)) {
+      return isEqual(arrowType.left, varAssignment.type);
+    }
+  }
+
+  // Bound variable not in premise context
+  return false;
+}
+
+export { action, arrowIntroduction };
--- test/verifiers/lambda.test.ts
+++ test/verifiers/lambda.test.ts
@@ -1,20 +1,18 @@
 import { Argument } from "@/lib/parsers/argument";
 import { Assignment, VarAssignment } from "@/lib/parsers/assignment";
 import { Context } from "@/lib/parsers/context";
-import { Variable } from "@/lib/parsers/lambda";
+import { Abstraction, Variable } from "@/lib/parsers/lambda";
 import { Arrow, TypeVar } from "@/lib/parsers/type";
-import { action } from "@/lib/verifiers/lambda";
+import { action, arrowIntroduction } from "@/lib/verifiers/lambda";
 
 test("Verifies action rule", () => {
   // x: 1 |- x: 1 is true
-  expect(
-    action(
-      new Argument(
-        new Context([new VarAssignment(new Variable("x"), new TypeVar(1))]),
-        new Assignment(new Variable("x"), new TypeVar(1)),
-      ),
-    ),
-  ).toBe(true);
+  const xOneXOne = new Argument(
+    new Context([new VarAssignment(new Variable("x"), new TypeVar(1))]),
+    new Assignment(new Variable("x"), new TypeVar(1)),
+  );
+
+  expect(action(xOneXOne, [])).toBe(true);
 
   // x: 1 |- x : 2 is false
   expect(
@@ -23,8 +21,12 @@ test("Verifies action rule", () => {
         new Context([new VarAssignment(new Variable("x"), new TypeVar(1))]),
         new Assignment(new Variable("x"), new TypeVar(2)),
       ),
+      [],
     ),
   ).toBe(false);
+
+  // x: 1 |- x : 1 with premises is false
+  expect(action(xOneXOne, [xOneXOne])).toBe(false);
 
   // x: 1 -> 2 |- x: 1 -> 2 is true
   expect(
@@ -33,6 +35,152 @@ test("Verifies action rule", () => {
         new Context([new VarAssignment(new Variable("x"), new Arrow(new TypeVar(1), new TypeVar(2)))]),
         new Assignment(new Variable("x"), new Arrow(new TypeVar(1), new TypeVar(2))),
       ),
+      [],
     ),
   ).toBe(true);
+});
+
+test("Verifies arrow introduction", () => {
+  // \emptyset |- \x. x: 1 -> 1 given x: 1 |- x : 1 is true
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context(),
+        new Assignment(
+          new Abstraction(new Variable("x"), new Variable("x")),
+          new Arrow(new TypeVar(1), new TypeVar(1)),
+        ),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("x"), new TypeVar(1))]),
+          new Assignment(new Variable("x"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(true);
+
+  // \emptyset |- \x. x: 2 -> 1 given x: 2 |- x : 1 is true (validity of conclusion independent from validity of premise)
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context(),
+        new Assignment(
+          new Abstraction(new Variable("x"), new Variable("x")),
+          new Arrow(new TypeVar(2), new TypeVar(1)),
+        ),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("x"), new TypeVar(2))]),
+          new Assignment(new Variable("x"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(true);
+
+  // x: 1 |- \x. x: 1 -> 1 given x: 1 |- x : 1 is false (bound variable in context)
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context([new VarAssignment(new Variable("x"), new TypeVar(1))]),
+        new Assignment(
+          new Abstraction(new Variable("x"), new Variable("x")),
+          new Arrow(new TypeVar(1), new TypeVar(1)),
+        ),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("x"), new TypeVar(1))]),
+          new Assignment(new Variable("x"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(false);
+
+  // \emptyset |- \x. x: 1 -> 1 given x: 2 |- x : 1 is false (bound variable type mismatch)
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context(),
+        new Assignment(
+          new Abstraction(new Variable("x"), new Variable("x")),
+          new Arrow(new TypeVar(1), new TypeVar(1)),
+        ),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("x"), new TypeVar(2))]),
+          new Assignment(new Variable("x"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(false);
+
+  // \emptyset |- \x. x: 1 given x: 1 |- x : 1 is false (not arrow type)
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context(),
+        new Assignment(new Abstraction(new Variable("x"), new Variable("x")), new TypeVar(1)),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("x"), new TypeVar(2))]),
+          new Assignment(new Variable("x"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(false);
+
+  // \emptyset |- \x. x: 1 -> 1 given y: 1 |- x : 1 is false (bound variable mismatch)
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context(),
+        new Assignment(
+          new Abstraction(new Variable("x"), new Variable("x")),
+          new Arrow(new TypeVar(1), new TypeVar(1)),
+        ),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("y"), new TypeVar(1))]),
+          new Assignment(new Variable("x"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(false);
+
+  // \emptyset |- \x. x: 1 -> 1 given x: 1 |- y : 1 is false (body mismatch)
+  expect(
+    arrowIntroduction(
+      new Argument(
+        new Context(),
+        new Assignment(
+          new Abstraction(new Variable("x"), new Variable("x")),
+          new Arrow(new TypeVar(1), new TypeVar(1)),
+        ),
+      ),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("y"), new TypeVar(1))]),
+          new Assignment(new Variable("y"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(false);
+
+  // \emptyset |- x: 1 -> 1 given x: 1 |- x : 1 is false (not an abstraction)
+  expect(
+    arrowIntroduction(
+      new Argument(new Context(), new Assignment(new Variable("x"), new Arrow(new TypeVar(1), new TypeVar(1)))),
+      [
+        new Argument(
+          new Context([new VarAssignment(new Variable("y"), new TypeVar(1))]),
+          new Assignment(new Variable("y"), new TypeVar(1)),
+        ),
+      ],
+    ),
+  ).toBe(false);
 });
