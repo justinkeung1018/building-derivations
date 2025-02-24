4fba596b149a439728b530a7e699f985a583157f
Justin Keung <justinkeung1018@gmail.com>
2025-02-24 16:47:28 +0000

    Remove baseUrl from Cypress config and pass in as env var for dev testing instead

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8000",
     specPattern: "test/frontend/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "test/frontend/support/e2e.{js,jsx,ts,tsx}",
   },
