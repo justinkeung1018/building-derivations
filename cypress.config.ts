990123e6cd9ee44f0f8a8a907515ba206e7b5470
Justin Keung <justinkeung1018@gmail.com>
2025-02-24 11:20:58 +0000

    Add dummy Cypress test and GitHub Action

---
+++ cypress.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    specPattern: "test/frontend/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "test/frontend/support/e2e.{js,jsx,ts,tsx}"
+  },
+})
