5ba4c10bd4f1d15976972d75399649316ec9fec1
Justin Keung <justinkeung1018@gmail.com>
2025-02-24 11:22:31 +0000

    Reformat

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     specPattern: "test/frontend/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "test/frontend/support/e2e.{js,jsx,ts,tsx}"
+    supportFile: "test/frontend/support/e2e.{js,jsx,ts,tsx}",
   },
-})
+});
