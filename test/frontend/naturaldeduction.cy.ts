61bb5ad5017dcd2cf1524b20c0f3bb83c3251c9d
Justin Keung <justinkeung1018@gmail.com>
2025-03-07 12:28:18 +0000

    Add comment on .click()

--- test/frontend/naturaldeduction.cy.ts
+++ test/frontend/naturaldeduction.cy.ts
@@ -42,7 +42,7 @@ it("correctly verifies a basic derivation tree with natural deduction rules", ()
   cy.clickOutside();
 
   // Construct a correct derivation tree
-  cy.getBySel("tree-conclusion-0").click().type("z, (z -> y) |- (x -> y)");
+  cy.getBySel("tree-conclusion-0").click().type("z, (z -> y) |- (x -> y)"); // Need to .click() otherwise the text won't be typed and the test will fail
   cy.getBySel("tree-rule-0").type("\\to I");
   cy.getBySel("tree-add-premise-button-0").click();
   cy.getBySel("tree-conclusion-1").type("x, z, (z -> y) |- y");
