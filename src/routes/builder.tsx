48a793875165b5edaf16f030a472d5264be0bbc2
Justin Keung <justinkeung1018@gmail.com>
2025-05-17 17:09:48 +0100

    Set valid properly in builder, delete commented code in MessageMap

--- src/routes/builder.tsx
+++ src/routes/builder.tsx
@@ -31,21 +31,26 @@ export const Route = createFileRoute("/builder")({
   validateSearch: searchSchema,
 });
 
+interface Errors {
+  inputErrors: MessageMap;
+  ruleErrors: MessageMap;
+}
+
 export function DerivationBuilder() {
   const search = Route.useSearch();
 
+  const [valid, setValid] = useState<boolean>(false);
   const [states, setStates] = useState<Record<number, ArgumentInputState>>({ 0: getDefaultState(0, null) });
-  const [inputErrors, setInputErrors] = useState(new MessageMap());
-  const [ruleErrors, setRuleErrors] = useState(new MessageMap());
+  const [{ inputErrors, ruleErrors }, setErrors] = useState({
+    inputErrors: new MessageMap(),
+    ruleErrors: new MessageMap(),
+  });
   const [syntax, setSyntax] = useState<SyntaxRule[]>([{ ...defaultSyntaxRule }]);
   const [inferenceRules, setInferenceRules] = useState<InferenceRule[]>([]);
 
-  // TODO: can we make valid stay false throughout initialisation?
-  const valid = inputErrors.size === 0 && ruleErrors.size === 0;
-
-  function verifyInput(index: number) {
+  function verifyInput(index: number, inputErrors: MessageMap, ruleErrors: MessageMap): Errors {
     for (const premiseIndex of states[index].premiseIndices) {
-      verifyInput(premiseIndex);
+      verifyInput(premiseIndex, inputErrors, ruleErrors);
     }
 
     const conclusion = states[index].conclusionInputState.value;
@@ -54,44 +59,37 @@ export function DerivationBuilder() {
     const rule = inferenceRules.find((rule) => states[index].ruleNameInputState.value === rule.name);
 
     if (rule === undefined) {
-      setRuleErrors((old) => {
-        old.push(index, "Undefined rule");
-        return old;
-      });
-      return;
-    }
+      ruleErrors.push(index, "Undefined rule");
+    } else {
+      const {
+        conclusionErrors,
+        ruleErrors: ruleErrorsList,
+        premisesErrors,
+      } = verify(conclusion, premises, rule, syntax);
 
-    const { conclusionErrors, ruleErrors: ruleErrorsList, premisesErrors } = verify(conclusion, premises, rule, syntax);
-
-    for (const message of conclusionErrors) {
-      setInputErrors((old) => {
-        old.push(index, message);
-        return old;
-      });
-    }
-
-    premisesErrors.forEach((messages, i) => {
-      const premiseIndex = states[index].premiseIndices[i];
-      for (const message of messages) {
-        setInputErrors((old) => {
-          old.push(premiseIndex, message);
-          return old;
-        });
+      for (const message of conclusionErrors) {
+        inputErrors.push(index, message);
       }
-    });
 
-    for (const message of ruleErrorsList) {
-      setRuleErrors((old) => {
-        old.push(index, message);
-        return old;
+      premisesErrors.forEach((messages, i) => {
+        const premiseIndex = states[index].premiseIndices[i];
+        for (const message of messages) {
+          inputErrors.push(premiseIndex, message);
+        }
       });
+
+      for (const message of ruleErrorsList) {
+        ruleErrors.push(index, message);
+      }
     }
+
+    return { inputErrors, ruleErrors };
   }
 
   useEffect(() => {
-    setInputErrors(new MessageMap());
-    setRuleErrors(new MessageMap());
-    verifyInput(0);
+    const errors = verifyInput(0, new MessageMap(), new MessageMap());
+    setErrors(errors);
+    setValid(errors.inputErrors.size === 0 && errors.ruleErrors.size === 0);
   }, [states]);
 
   useEffect(() => {
