e889c2c9429a61e035ca4b3d4f903c02901e9285
Justin Keung <justinkeung1018@gmail.com>
2025-05-31 19:04:22 +0100

    Blurs input when user presses enter

--- src/components/shadcn/Input.tsx
+++ src/components/shadcn/Input.tsx
@@ -7,6 +7,12 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
     return (
       <input
         type={type}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            e.currentTarget.blur();
+          }
+        }}
         className={cn(
           "flex items-center h-9 w-80 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className,
